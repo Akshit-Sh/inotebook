50824537ffe717ecd9718fff3bed2554ccdb68ed
Akshit <akshitsharma20022@gmail.com>
2022-08-23 23:02:10 +0530

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+const connectTOmongoose = require('./db');
+const express = require('express')
+connectTOmongoose();
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Akshit!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
